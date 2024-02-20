d40efd3a9d0d43b339541554fe0c3c73d9bef5ae
Muraru Mihai <mihai.muraru@open.ro>
2024-02-20 17:00:28 +0200

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
 
   return (
     <>
-      <Button colorScheme='blue'>Button</Button>
+        <Grid templateAreas={{
+            base: `"nav" "main"`, // mobile
+            lg: `"nav nav" "aside main"` //> 1024px
+        }}>
+            <GridItem area='nav' bg={'coral'}>
+                NAV
+            </GridItem>
+            <Show above='lg'>  
+                <GridItem area='aside' bg={'gold'}>
+                    Aside
+                </GridItem>
+            </Show>
+            
+            <GridItem area='main' bg={'dodgerblue'}>
+                main
+            </GridItem>
+        </Grid>
     </>
   )
 }
