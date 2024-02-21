ce58aab7de510ca48e2203d8bf6f9178d460e033
Muraru Mihai <mihai.muraru@open.ro>
2024-02-21 12:29:42 +0200

    Build color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
+import ColorModeSwitch from './ColorModeSwitch';
 
 const NavBar = () => {
   return (
-    <HStack> // horizilthat stack
+    <HStack justifyContent='space-between' padding='10px'> // horizilthat stack
         <Image src={logo} boxSize={'60px'}/>
         <Text>Navbar</Text>
+        <ColorModeSwitch />
     </HStack>
   )
 }
