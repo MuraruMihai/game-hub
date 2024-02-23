e228fccfd6e26cf0d9bf04742f8a5ffcb268dc7a
Muraru Mihai <mihai.muraru@open.ro>
2024-02-23 15:05:53 +0200

    Highlight selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,9 +5,10 @@ import GenreListItemSkeleton from './GenreListItemSkeleton';
 
 interface Props{
     onSelectGenre: (genre: Genre) => void,
+    selectedGenre: Genre | null
 }
 
-const GenreList = ({onSelectGenre} : Props) => {
+const GenreList = ({selectedGenre, onSelectGenre} : Props) => {
 
     const {data, isLoading, error} = useGenres();
     const skeletonData = [1,2,3,4,5,6,7]
@@ -17,13 +18,15 @@ const GenreList = ({onSelectGenre} : Props) => {
         console.log(genre.name)
         onSelectGenre(genre);
     }
+    if(isLoading) return <List>{skeletonData.map(skeleton => <GenreListItemSkeleton key={skeleton}/>)}</List>
+
   return (
     <List>
-        {isLoading && skeletonData.map(skeleton => <GenreListItemSkeleton key={skeleton}/>)}
+        
         {data.map(genre => <ListItem padding={1} key={genre.id}>
             <HStack>
                 <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
-                <Button fontSize='lg' variant='link' onClick={() => onGenreClick(genre)}>{genre.name}</Button>
+                <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} fontSize='lg' variant='link' onClick={() => onGenreClick(genre)}>{genre.name}</Button>
             </HStack>
         </ListItem>)}
     </List>
