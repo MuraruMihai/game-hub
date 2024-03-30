923bf89c1ac9c3124c493c8f53351b76db108855
Muraru Mihai <mihai.muraru@open.ro>
2024-03-30 12:43:22 +0200

    Refactor = Move entities interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,14 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import APIClient from '../services/api-client';
 import genres from '../data/genres';
-
-export interface Genre {
-    id: number,
-    name: string,
-    slug: string,
-    games_count: number,
-    image_background: string
-}
+import { Genre } from '../entities/Genre';
 
 const apiClient = new APIClient<Genre>('/genres');
 
